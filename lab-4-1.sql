27354b56019b4c769bee7280a5763553cfc7a00c
Tigran Tyler Pell <tigran.tylerpell@kellogg.northwestern.edu>
2025-01-14 21:15:15 +0000

    lab 4.1 done

--- lab-4-1.sql
+++ lab-4-1.sql
@@ -3,4 +3,7 @@
 -- Expected result:
 -- 2935
 
-
+SELECT players.first_name, players.last_name, sum(stats.hits)
+FROM stats INNER JOIN players on players.id = stats.player_id
+WHERE players.first_name == "Barry" AND last_name == "Bonds"
+GROUP BY players.id
